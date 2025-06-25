3f23f7e3b64ac2d0ee8b6a0758317cb313d0f44e
Vikas Tiwari <54770104+VikasTiwari26@users.noreply.github.com>
2025-06-25 22:14:56 +0530

    Add files via upload
    icons and banners added

---
+++ src/img/images.js
@@ -0,0 +1,31 @@
+import githubIcon from './icons/git.png'
+import reactIcon from './icons/react.png'
+import angularIcon from './icons/angularjs.png'
+import angularTextIcon from './icons/angularjs-text.png'
+import ionicIcon from './icons/ionic.png'
+import ionicTextIcon from './icons/ionic-text.png'
+import jsIcon from './icons/javascript.png'
+import htmlIcon from './icons/html5.png'
+import cssIcon from './icons/css3.png'
+import bootstrapIcon from './icons/bootstrap.png'
+import nodeJsIcon from './icons/node-js.png'
+import javaIcon from './icons/java.png'
+import awsIcon from './icons/amazonwebservices.png'
+import nextJsIcon from './icons/next-js.png'
+
+export {
+    githubIcon,
+    reactIcon,
+    angularIcon,
+    angularTextIcon,
+    ionicIcon,
+    ionicTextIcon,
+    jsIcon,
+    htmlIcon,
+    cssIcon,
+    bootstrapIcon,
+    nodeJsIcon,
+    javaIcon,
+    awsIcon,
+    nextJsIcon
+}
---
+++ src/img/portfolioImages.js
@@ -0,0 +1,135 @@
+import avivaDashboard from './projects/aviva/dashboard.jpg';
+import avivaMyProfile from './projects/aviva/my-profile.jpg';
+import avivaLanding from './projects/aviva/landing.jpg';
+import avivaGallery from './projects/aviva/gallery.jpg';
+import avivaGalleryAlbum from './projects/aviva/gallery-album.jpg';
+import avivaCustomerComm from './projects/aviva/Customer-Communication.jpg';
+import avivaKnowledgeCenterPL from './projects/aviva/Knowledge-Center--Product-List.jpg';
+import avivaKnowledgeCenter from './projects/aviva/knowledge-center.jpg';
+import avivaKnowledgeCenterVideo from './projects/aviva/knowledge-center-video-player.jpg';
+import avivaPersistency from './projects/aviva/Persistency.jpg';
+import avivaProjectedCommission from './projects/aviva/Projected-Commission.jpg';
+import avivaRegister from './projects/aviva/register.jpg';
+import avivaResetPassword from './projects/aviva/reset-password.jpg';
+import avivaSearchresults from './projects/aviva/Search-Result.jpg';
+import avivaToolsAddTask from './projects/aviva/tools-add-task.jpg';
+import avivaTools from './projects/aviva/tools.jpg';
+import avivaWhatsBuzzing from './projects/aviva/Whats-Buzzing.jpg';
+import mmmDashboard from './projects/mmm/home-page.png';
+import mmmOnboard from './projects/mmm/onboard.png';
+import mmmBlog from './projects/mmm/BLOG.jpeg';
+import mmmBlog1 from './projects/mmm/BLOG1.jpeg';
+import mmmBlog2 from './projects/mmm/BLOG2.jpeg';
+import mmmBlog3 from './projects/mmm/BLOG3.jpeg';
+import mmmCreditScore from './projects/mmm/CREDIT SCORE.jpeg';
+import mmmCreditScore1 from './projects/mmm/CREDIT SCORE1.jpeg';
+import mmmListingPage from './projects/mmm/LISTING_PAGE.jpeg';
+import mmmListingPage1 from './projects/mmm/LISTING_PAGE1.jpeg';
+import mmmListingPage2 from './projects/mmm/LISTING_PAGE5.jpeg';
+import mmmLongForm from './projects/mmm/LONG FORM.jpeg';
+import mmmLongForm1 from './projects/mmm/LONG FORM1.jpeg';
+import mmmLongForm2 from './projects/mmm/LONG FORM2.jpeg';
+import mmmLongForm3 from './projects/mmm/LONG FORM3.jpeg';
+import mmmLongForm4 from './projects/mmm/LONG FORM4.jpeg';
+import mmmLongFormThankYou from './projects/mmm/LONG FORM5_THNQ.jpeg';
+import mmmShortForm from './projects/mmm/SHORT FORM.jpeg';
+import mmmShortForm1 from './projects/mmm/SHORT FORM1.jpeg';
+import mmmShortForm2 from './projects/mmm/SHORT FORM2.jpeg';
+import mmmShortForm3 from './projects/mmm/SHORT FORM3.jpeg';
+import mmmShortForm4 from './projects/mmm/SHORT FORM4.jpeg';
+import mmmShortForm5 from './projects/mmm/SHORT FORM5.jpeg';
+import mloDashboard from './projects/mlo/mloDashboard.png';
+import chatbotLanding from './projects/chatbot/chatbot.png';
+import adminLogin from './projects/posv-admin/AdminPanelLogin.png';
+import posvLanding from './projects/posv/posv-landing.png';
+import posv1 from './projects/posv/posv1.png';
+import posv2 from './projects/posv/posv2.png';
+import posv3 from './projects/posv/posv3.png';
+import posv4 from './projects/posv/posv4.png';
+import posv5 from './projects/posv/posv5.png';
+import posv6 from './projects/posv/posv6.png';
+import posv7 from './projects/posv/posv7.png';
+import posv8 from './projects/posv/posv8.png';
+import posv9 from './projects/posv/posv9.png';
+import posv10 from './projects/posv/posv10.png';
+import posv11 from './projects/posv/posv11.png';
+import posv12 from './projects/posv/posv12.png';
+import posv13 from './projects/posv/posv13.png';
+import posv14 from './projects/posv/posv14.png';
+import posv15 from './projects/posv/posv15.png';
+import posv16 from './projects/posv/posv16.png';
+import posv17 from './projects/posv/posv17.png';
+import posv18 from './projects/posv/posv18.png';
+import posv19 from './projects/posv/posv19.png';
+import gotDashboard from './projects/got/dashboard.png'
+
+
+export const dashboards = { avivaDashboard, mmmDashboard, mloDashboard, adminLogin, posvLanding, gotDashboard,chatbotLanding }
+
+export const avivaImages = [
+    avivaDashboard,
+    avivaLanding,
+    avivaRegister,
+    avivaResetPassword,
+    avivaMyProfile,
+    avivaGallery,
+    avivaGalleryAlbum,
+    avivaProjectedCommission,
+    avivaPersistency,
+    avivaTools,
+    avivaToolsAddTask,
+    avivaCustomerComm,
+    avivaKnowledgeCenter,
+    avivaKnowledgeCenterVideo,
+    avivaKnowledgeCenterPL,
+    avivaSearchresults,
+    avivaWhatsBuzzing
+]
+
+export const mmmImages = [
+    mmmDashboard,
+    mmmOnboard,
+    mmmBlog,
+    mmmBlog1,
+    mmmBlog2,
+    mmmBlog3,
+    mmmCreditScore,
+    mmmCreditScore1,
+    mmmListingPage,
+    mmmListingPage1,
+    mmmListingPage2,
+    mmmLongForm,
+    mmmLongForm1,
+    mmmLongForm2,
+    mmmLongForm3,
+    mmmLongForm4,
+    mmmLongFormThankYou,
+    mmmShortForm,
+    mmmShortForm1,
+    mmmShortForm2,
+    mmmShortForm3,
+    mmmShortForm4,
+    mmmShortForm5
+]
+
+export const posvImages = [
+    posv1,
+    posv2,
+    posv3,
+    posv4,
+    posv5,
+    posv6,
+    posv7,
+    posv8,
+    posv9,
+    posv10,
+    posv11,
+    posv12,
+    posv13,
+    posv14,
+    posv15,
+    posv16,
+    posv17,
+    posv18,
+    posv19,
+]
